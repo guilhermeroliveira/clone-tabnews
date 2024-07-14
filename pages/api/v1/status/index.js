16fb9d29ed952a92a3a65c50c319087740acd748
Guilherme Ribeiro <59616204+guilhermeroliveira@users.noreply.github.com>
2024-07-14 15:52:01 -0300

    \add `database.js' and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  await database.query("SELECT 1 as res;");
   response.status(200).json({ "mensagem": "alunos do curso.dev são pessoas acima da média" });
 }
 
