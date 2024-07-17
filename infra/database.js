b8ff37c309e37d7cb5d39db2dbaa1665d13f8a5b
Guilherme Ribeiro <59616204+guilhermeroliveira@users.noreply.github.com>
2024-07-17 18:43:24 -0300

    improvement: handle connection errors on  module

--- infra/database.js
+++ infra/database.js
@@ -6,15 +6,18 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.POSTGRES_USE_SSL === 'true'
   });
-  await client.connect();
 
   try {
+    await client.connect();
+
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
