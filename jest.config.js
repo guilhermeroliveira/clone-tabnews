2ff5ffe029ee25de5629943fafebb1865a8f4ad1
Guilherme Ribeiro <59616204+guilhermeroliveira@users.noreply.github.com>
2024-08-02 17:49:58 -0300

    test: improve robustness of `npm test` script with `async-retry` module and `orchestrator.js`

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,17 @@
 const dotenv = require("dotenv");
+const nextJest = require("next/jest");
+
 dotenv.config({
   path: ".env.development"
 });
 
-const nextJest = require("next/jest");
-
 const createJestConfig = nextJest({
   dir: "."
 });
+
 const jestConfig = createJestConfig({
-  moduleDirectories: ["node_modules", "<rootDir>"]
+  moduleDirectories: ["node_modules", "<rootDir>"],
+  testTimeout: 60000
 });
 
 module.exports = jestConfig;
