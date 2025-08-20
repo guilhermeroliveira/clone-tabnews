b62c6b445a7b888510c9c8691db7e954266899df
Guilherme Ribeiro <59616204+guilhermeroliveira@users.noreply.github.com>
2025-08-19 21:28:36 -0300

    feat(user): add GET `/user` endpoint

---
+++ pages/api/v1/user/index.js
@@ -0,0 +1,24 @@
+import controller from "infra/controller";
+import session from "model/session";
+import user from "model/user";
+import { createRouter } from "next-connect";
+
+const router = createRouter();
+
+router.get(getHandler);
+
+async function getHandler(request, response) {
+  const sessionToken = request.cookies.session_id;
+
+  const foundSession = await session.findOneValidByToken(sessionToken);
+  const updatedSession = await session.renew(foundSession.id);
+  controller.setSessionCookie(updatedSession.token, response);
+
+  const foundUser = await user.findOneById(foundSession.user_id);
+
+  response.setHeader("Cache-Control", "no-store, no-cache, max-age=0, must-revalidate");
+
+  return response.status(200).json(foundUser);
+}
+
+export default router.handler(controller.errorHandlers);
